92d738ba2338e3230cb38137ac9bc582faf2c12a
safeenataj <shama.safeenataj@gmail.com>
2024-10-29 11:01:01 +0100

    changes in userrep and advetise rep

--- src/main/java/lexicon/se/market_api/repository/AdvertisementRepository.java
+++ src/main/java/lexicon/se/market_api/repository/AdvertisementRepository.java
@@ -7,11 +7,13 @@ package lexicon.se.market_api.repository;
 import lexicon.se.market_api.domain.Advertisement;
 import lexicon.se.market_api.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface AdvertisementRepository extends JpaRepository<Advertisement, Long> {
     List<Advertisement> findByUser(User user);
     List<Advertisement> findByExpiredDateBefore(LocalDateTime now);
 }
-
--- src/main/java/lexicon/se/market_api/repository/UserRepository.java
+++ src/main/java/lexicon/se/market_api/repository/UserRepository.java
@@ -2,10 +2,14 @@ package lexicon.se.market_api.repository;
 
 
 
-import com.marketplace.entity.User;
+
+import lexicon.se.market_api.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<lexicon.se.market_api.repository.User> findByEmail(String email);
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
