1d14573628adc25b5d7679cdbea3b504b929691d
safeenataj <shama.safeenataj@gmail.com>
2024-10-29 11:00:09 +0100

    changes in userdto and advertise

--- src/main/java/lexicon/se/market_api/domain/dto/AdvertisementDTO.java
+++ src/main/java/lexicon/se/market_api/domain/dto/AdvertisementDTO.java
@@ -3,12 +3,34 @@ package lexicon.se.market_api.domain.dto;
 
 import java.time.LocalDateTime;
 
-public class AdvertisementDTO {
-    private String title;
-    private String description;
-    private double price;
-    private String category;
-    private LocalDateTime expiredDate;
-    // Getters, Setters
-}
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class AdvertisementDTO {
+
+    @NotBlank(message = "Title cannot be empty.")
+    private String title;
+
+    @NotBlank(message = "Description cannot be empty.")
+    private String description;
+
+    @Positive(message = "Price must be a positive number.")
+    private double price;
+
+    @NotBlank(message = "Category cannot be empty.")
+    private String category;
+
+    @NotNull(message = "Expired date cannot be null.")
+    private LocalDateTime expiredDate;
+}
--- src/main/java/lexicon/se/market_api/domain/dto/UserDTO.java
+++ src/main/java/lexicon/se/market_api/domain/dto/UserDTO.java
@@ -1,14 +1,28 @@
 package lexicon.se.market_api.domain.dto;
 
 
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
 public class UserDTO {
+
+    @NotBlank(message = "Email cannot be empty.")
+    @Email(message = "Must be a valid email address.")
     private String email;
+
+    @NotBlank(message = "Password cannot be empty.")
+    @Size(min = 8, message = "Password must be at least 8 characters long.")
     private String password;
 
-    public Object getEmail() {
-    }
-
-    public Object getPassword() {
-    }
-    // Getters, Setters
+    private boolean isActive;
 }
+
