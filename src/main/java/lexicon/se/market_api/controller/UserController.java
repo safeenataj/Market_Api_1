a74f01682c282eeec2df30cd828a2440526e0346
safeenataj <shama.safeenataj@gmail.com>
2024-10-30 16:13:55 +0100

    changes

--- src/main/java/lexicon/se/market_api/controller/UserController.java
+++ src/main/java/lexicon/se/market_api/controller/UserController.java
@@ -1,26 +1,33 @@
 package lexicon.se.market_api.controller;
-
-
-
-import lexicon.se.market_api.domain.User;
-import lexicon.se.market_api.domain.dto.UserDTO;
+import jakarta.validation.Valid;
+import lexicon.se.market_api.domain.dto.UserDTOView;
+import lexicon.se.market_api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/v1/users")
 public class UserController {
 
+    private final UserService userService;
+
     @Autowired
-    private UserService userService;
-
-    @PostMapping("/register")
-    public User registerUser(@RequestBody UserDTO userDTO) {
-        return userService.registerUser(userDTO);
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
-    @PostMapping("/login")
-    public User loginUser(@RequestBody UserDTO userDTO) {
-        return userService.authenticateUser(userDTO.getEmail(), userDTO.getPassword());
+    @PostMapping
+    public ResponseEntity<Void> registerUser(@RequestBody @Valid UserDTOView userDTO) {
+        userService.registerUser(userDTO); // You might need to convert DTO to Entity
+        return ResponseEntity.noContent().build();
     }
+
+  /*  @GetMapping("/{id}")
+    public ResponseEntity<UserDTOView> getUserById(@PathVariable String id) {
+        UserDTOView user = userService.getUserById(id); // Retrieve user by ID
+        return ResponseEntity.ok(user);
+    }*/
+
+    // Additional user-related endpoints can be added here
 }
